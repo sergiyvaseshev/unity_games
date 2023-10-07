887e1dbb793b2de7cdd7b4b85d1a7714e28f9134
sergiyvaseshev <>
2023-10-07 12:36:01 +0300

    add moterial

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float powerupStrength=15f;
+    public bool hasPowerup;
+    public float speed;
+    public float respawnPos;
+    public Rigidbody rigidbody;
+    private GameObject focalPoint;
+    public GameObject powerup;
+    public GameObject cars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cars.gameObject.transform.position= transform.position;
+        if(transform.position.y< respawnPos)
+        {
+            rigidbody.velocity = Vector3.zero;
+            transform.position = Vector3.zero;
+        }
+        float forwardInput = Input.GetAxis("Vertical");
+        rigidbody.AddForce(focalPoint.transform.forward* speed * forwardInput);
+
+        powerup.gameObject.transform.position = transform.position + new Vector3(0, -0.1f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {                    
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupRoutine());
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+           Debug.Log("Collided with " + collision.gameObject.name + " hasPower = " + hasPowerup);
+           if(hasPowerup == true)
+            {
+                Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+                Vector3 awayFromPlayer=collision.gameObject.transform.position-transform.position;
+
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+
+        }
+    }
+    private IEnumerator PowerupRoutine() 
+    {
+        hasPowerup = true;
+        powerup.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(7);
+
+        hasPowerup = false;
+        powerup.gameObject.SetActive(false);
+    }
+}       
